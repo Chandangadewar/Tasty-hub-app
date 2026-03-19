d9d71a631a5494ef880ce2ea38373d3da6e8f2f3
Chandan gadewar <144953053+Chandangadewar@users.noreply.github.com>
2026-03-19 16:33:05 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -5,18 +5,18 @@ resource "aws_instance" "tastyhub_server" {
   vpc_security_group_ids = [aws_security_group.tastyhub_sg.id]
 
   user_data = <<-EOF
-    #!/bin/bash
-    apt-get update -y
-    apt-get install -y docker.io docker-compose git
-    systemctl start docker
-    systemctl enable docker
-    usermod -aG docker ubuntu
+#!/bin/bash
+apt-get update -y
+apt-get install -y docker.io docker-compose git
+systemctl start docker
+systemctl enable docker
+usermod -aG docker ubuntu
 
-    cd /home/ubuntu
-    git clone https://github.com/Chandangadewar/Tasty-hub-app.git
-    chown -R ubuntu:ubuntu /home/ubuntu/Tasty-hub-app
+cd /home/ubuntu
+git clone https://github.com/Chandangadewar/Tasty-hub-app.git
+chown -R ubuntu:ubuntu /home/ubuntu/Tasty-hub-app
 
-    cat > /home/ubuntu/Tasty-hub-app/docker/.env << 'ENVEOF'
+cat > /home/ubuntu/Tasty-hub-app/docker/.env << 'ENVEOF'
 DB_ROOT_PASSWORD=rootpassword123
 DB_NAME=restaurant_db
 DB_USER=tastyhub
@@ -26,9 +26,9 @@ DD_API_KEY=dummy
 DOCKER_USERNAME=chandan240603
 ENVEOF
 
-    cd /home/ubuntu/Tasty-hub-app/docker
-    docker-compose up -d
-  EOF
+cd /home/ubuntu/Tasty-hub-app/docker
+docker-compose up -d
+EOF
 
   tags = {
     Name    = "TastyHub-Server"
